2dc7825e83664bf3fda9ffdb548c6f29df63ac26
“Helen” <25727036+helenzhou6@users.noreply.github.com>
2025-07-07 13:06:12 +0100

    Create a pickle file and save it to data directory of the dataset

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,23 @@
 from datasets import load_dataset
+import numpy as np
+import pickle
+import os
+
+NUM_SAMPLES = 100
 
 train_us_dataset = load_dataset("danavery/urbansound8K", split="train")
 
-print(train_us_dataset.column_names)
-for i in range(3):
-    print(train_us_dataset[i])
+samples = []
+for sample_row in train_us_dataset.select(range(NUM_SAMPLES)):
+    data = {
+        "audio": np.array(sample_row["audio"]["array"]),
+        "target_label": sample_row["class"]
+    }
+    samples.append(data)
+
+if not os.path.exists("data"):
+    os.makedirs("data")
+
+with open("data/urbansound_samples.pkl.gz", "wb") as file:
+    pickle.dump(samples, file)
+
